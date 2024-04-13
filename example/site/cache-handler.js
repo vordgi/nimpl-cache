d9c518e8f141df5f490731cb14344ec0c6010e5f
Vordgi <sasha2822222@gmail.com>
2024-04-13 20:13:13 +0400

    nic-42 update example package imports

--- example/site/cache-handler.js
+++ example/site/cache-handler.js
@@ -1,17 +1,18 @@
 // @ts-check
-const AppAdapter = require('@nimpl/cache-adapter');
-const CacheHandler = require('@nimpl/cache-in-memory');
+const { AppAdapter } = require("@nimpl/cache-adapter");
+const CacheHandler = require("@nimpl/cache-in-memory");
 
 class CustomCacheHandler extends AppAdapter {
     /** @param {any} options */
     constructor(options) {
         super({
+            // @ts-expect-error temp error ignoring
             CacheHandler,
-            buildId: process.env.BUILD_ID || 'base_id',
-            cacheUrl: 'http://localhost:4000',
-            cacheMode: 'remote',
+            buildId: process.env.BUILD_ID || "base_id",
+            cacheUrl: "http://localhost:4000",
+            cacheMode: "remote",
             options,
-        })
+        });
     }
 }
 
